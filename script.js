a0a25f2f955b6672468e2e9a1003bd279ccb74b5
astrocoding <reepalstudio01@gmail.com>
2024-07-30 17:44:36 +0700

    add JQuery script

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+  $("#calculateTotal").click(function() {
+      let nasiGorengPrice = 10000;
+      let ayamGorengPrice = 12000;
+      let esTehPrice = 2000;
+      let kopiPrice = 3000;
+      
+      let nasiGorengQty = parseInt($("#nasiGorengQty").val()) || 0;
+      let ayamGorengQty = parseInt($("#ayamGorengQty").val()) || 0;
+      let esTehQty = parseInt($("#esTehQty").val()) || 0;
+      let kopiQty = parseInt($("#kopiQty").val()) || 0;
+      
+      let totalPrice = (nasiGorengPrice * nasiGorengQty) +
+                       (ayamGorengPrice * ayamGorengQty) +
+                       (esTehPrice * esTehQty) +
+                       (kopiPrice * kopiQty);
+      
+      $("#totalPrice").text("Total Harga: Rp " + totalPrice.toLocaleString("id-ID"));
+  });
+});
